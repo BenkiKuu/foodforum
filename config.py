654f4291797ca35af1c9b007c5a2dd1146c527cb
BenkiKuu <iganeleo@gmail.com>
2018-07-05 11:49:53 +0300

    edits config to fix a bug

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     """
     General configuration parent class
     """
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://username:password@localhost/foodforum'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SECRET_KEY=os.environ.get('SECRET_KEY')
 
